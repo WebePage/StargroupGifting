92c5b14d3d0c6354ff0d5e6ef3d7c0edebded1ed
AnuhasLiyanage <99825573+AnuhasLiyanage@users.noreply.github.com>
2022-02-18 00:04:57 +0530

    Add files via upload

---
+++ stepwork.js
@@ -0,0 +1,14 @@
+function workaa()
+{
+    document.getElementById('next').classList.add('nextre')
+    document.getElementById('test').classList.add('long')
+    document.getElementById('test2').classList.add('long2')
+    document.getElementById('two').classList.add('twodis')
+    document.getElementById('lap').classList.add('lapdis')
+    document.getElementById('typelap').classList.add('typelapdis')
+    document.getElementById('opareting').classList.add('oparetingdis')
+    document.getElementById('typeoparething').classList.add('typeoparethingdis')
+    document.getElementById('saba').classList.add('sabadis')
+    document.getElementById('li11').classList.add('li1')
+    document.getElementById('li22').classList.add('li2')
+}
