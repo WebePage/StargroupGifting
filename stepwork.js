7cd83706e7dcbb3e41c04f2eded8b8ad8cfa68a7
AnuhasLiyanage <99825573+AnuhasLiyanage@users.noreply.github.com>
2022-02-18 18:32:07 +0530

    Add files via upload

--- stepwork.js
+++ stepwork.js
@@ -11,4 +11,33 @@ function workaa()
     document.getElementById('saba').classList.add('sabadis')
     document.getElementById('li11').classList.add('li1')
     document.getElementById('li22').classList.add('li2')
+    document.getElementById('kind').classList.add('kindful')
+}
+
+function scrool()
+{
+    document.getElementById('kind').classList.add('mostkind')
+    document.getElementById('li22').classList.remove('li2')
+}
+
+function remo(num)
+{
+    document.getElementById('kind').classList.remove('mostkind')
+    document.getElementById('li22').classList.add('li2')
+    document.getElementById('typelap').value = num
+}
+
+function down()
+{
+    document.getElementById('System').classList.add('Systemful')
+    document.getElementById('saba').classList.remove('sabadis')
+    document.getElementById('te').classList.add('lo')
+}
+
+function re(bers)
+{
+    document.getElementById('typeoparething').value = bers
+    document.getElementById('System').classList.remove('Systemful')
+    document.getElementById('saba').classList.add('sabadis')
+    document.getElementById('te').classList.remove('lo')
 }
